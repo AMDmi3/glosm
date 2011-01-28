6037a729915d3e0b58891a5d8e7c6a29b1768b68
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-01-28 11:26:28 +0300

    Expand view radius, enable async loading

--- viewer/Main.cc
+++ viewer/Main.cc
@@ -67,8 +67,8 @@ void Display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	if (layer_p) {
-		int radius = 100000;
-		layer_p->RequestVisible(BBoxi(viewer.GetPos(MercatorProjection()) - Vector2i(radius, radius), viewer.GetPos(MercatorProjection()) + Vector2i(radius, radius)), true);
+		int radius = 50000000;
+		layer_p->RequestVisible(BBoxi(viewer.GetPos(MercatorProjection()) - Vector2i(radius, radius), viewer.GetPos(MercatorProjection()) + Vector2i(radius, radius)), false);
 		layer_p->GarbageCollect();
 		layer_p->Render(viewer);
 	}
