60b2d58fe5db354b1389c8445a7445b45c491267
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-02-09 09:47:10 +0300

    Improve exception implementation
    Use custom buffer for safe and effecient stream-like exception support

--- libglosm-server/include/glosm/Exception.hh
+++ libglosm-server/include/glosm/Exception.hh
@@ -25,6 +25,86 @@
 
 namespace Private {
 	/**
+	 * Safe string buffer to be used in exceptions.
+	 *
+	 * As you may know, std::exception::what() is has no-throw
+	 * exception specifier, thus you should not throw in what(),
+	 * or else the program may terminate during exception
+	 * handling (even if all exceptions are handled properly).
+	 *
+	 * Stock IOstream stringbuf is unsafe from this point of view
+	 * as its str() method does construct string object which may
+	 * throw, so we use our own buffer here. It may be normally
+	 * used with std::ostream, yet it has safe c_str() method which
+	 * never throws. In addition to that, it handles space reserve
+	 * so data may be appended
+	 */
+	class SafeStringBuffer : public std::streambuf {
+	protected:
+		/** initial size of a buffer */
+		static const unsigned int initial_size_ = 64;
+
+	protected:
+		unsigned int reserve_;
+		unsigned int allocated_;
+		unsigned int used_;
+		char* buffer_;
+
+	protected:
+		/**
+		 * Ensures that buffer has enough space expanding it if necessary
+		 *
+		 * @param size required buffer size
+		 */
+		void EnsureSize(unsigned int size);
+
+	public:
+		SafeStringBuffer(unsigned int reserve = 0);
+		SafeStringBuffer(const char* message);
+		SafeStringBuffer(const SafeStringBuffer& s);
+		~SafeStringBuffer();
+
+		/**
+		 * Changes reserve for the buffer expanding it if necessry
+		 *
+		 * @param reserve new reserve value
+		 */
+		void SetReserve(unsigned int reserve);
+
+		/**
+		 * Append data to the buffer
+		 *
+		 * This function is used from ostream to append data to the buffer.
+		 *
+		 * @param s pointer to data
+		 * @param n data length
+		 *
+		 * @return number of bytes appended (always n)
+		 */
+		std::streamsize xsputn(const char* s, std::streamsize n);
+
+		/**
+		 * Safely append data to the buffer within the reserve
+		 *
+		 * This may be safely used in what() to append additional data
+		 * (such ass strerror) to the end of message.
+		 *
+		 * @param s pointer to data
+		 * @param n data length
+		 *
+		 * @return number of bytes appended
+		 */
+		std::streamsize AppendReserve(const char* s, std::streamsize n) throw();
+
+		/**
+		 * Safely get pointer to buffer contents
+		 *
+		 * @return pointer to null-terminated buffer contents
+		 */
+		const char* c_str() throw();
+	};
+
+	/**
 	 * Convenient exception class which supports stream-like appending
 	 *
 	 * Example:
@@ -32,7 +112,7 @@ namespace Private {
 	 */
 	class Exception: public std::exception {
 	protected:
-		mutable std::stringstream message_;
+		mutable SafeStringBuffer message_;
 
 	public:
 		Exception();
@@ -41,7 +121,8 @@ namespace Private {
 
 		template <class T>
 		void Append(const T& t) const {
-			(std::ostream&)message_ << t;
+			std::ostream stream(&message_);
+			(std::ostream&)stream << t;
 		}
 
 		virtual const char* what() const throw();
@@ -66,7 +147,6 @@ class Exception: public Private::Exception {
  */
 class SystemError: public Exception {
 protected:
-	mutable std::stringstream full_message_;
 	int errno_;
 
 public:
--- libglosm-server/src/Exception.cc
+++ libglosm-server/src/Exception.cc
@@ -19,35 +19,106 @@
 
 #include <glosm/Exception.hh>
 
-#include <errno.h>
-#include <stdio.h>
+#include <cerrno>
 
 namespace Private {
-	Exception::Exception(): message_(std::ios_base::out | std::ios_base::app) {
+	void SafeStringBuffer::EnsureSize(unsigned int size) {
+		unsigned int newallocated_ = allocated_;
+		while (size > newallocated_)
+			newallocated_ *= 2;
+
+		if (newallocated_ > allocated_) {
+			char* newbuffer = new char[newallocated_];
+			memcpy(newbuffer, buffer_, used_);
+			delete[] buffer_;
+			buffer_ = newbuffer;
+			allocated_ = newallocated_;
+		}
 	}
 
-	Exception::Exception(const Exception& e): std::exception(), message_(e.message_.str(), std::ios_base::out | std::ios_base::app) {
+	SafeStringBuffer::SafeStringBuffer(unsigned int reserve)
+		: reserve_(reserve),
+		  allocated_(initial_size_ + reserve),
+		  used_(0),
+		  buffer_(new char[allocated_]) {
+	}
+
+	SafeStringBuffer::SafeStringBuffer(const char* message)
+		: reserve_(0),
+		  allocated_(strlen(message) + 1),
+		  used_(allocated_ - 1),
+		  buffer_(new char[allocated_]) {
+		strcpy(buffer_, message);
+	}
+
+	SafeStringBuffer::SafeStringBuffer(const SafeStringBuffer& s)
+		: std::streambuf(),
+		  reserve_(s.reserve_),
+		  allocated_(s.allocated_),
+		  used_(s.used_),
+		  buffer_( new char[allocated_]) {
+		memcpy(buffer_, s.buffer_, used_);
+	}
+
+	SafeStringBuffer::~SafeStringBuffer() {
+		delete[] buffer_;
+	}
+
+	void SafeStringBuffer::SetReserve(unsigned int reserve) {
+		EnsureSize(used_ + reserve + 1);
+		reserve_ = reserve;
+	}
+
+	std::streamsize SafeStringBuffer::xsputn(const char* s, std::streamsize n) {
+		EnsureSize(used_ + n + reserve_ + 1);
+		memcpy(buffer_ + used_, s, n);
+		used_ += n;
+		return n;
+	}
+
+	std::streamsize SafeStringBuffer::AppendReserve(const char* s, std::streamsize n) throw() {
+		if (n > reserve_)
+			n = reserve_;
+
+		memcpy(buffer_ + used_, s, n);
+		used_ += n;
+		reserve_ -= n;
+
+		return n;
+	}
+
+	const char* SafeStringBuffer::c_str() throw() {
+		buffer_[used_] = '\0';
+		return buffer_;
+	}
+
+	Exception::Exception() {
+	}
+
+	Exception::Exception(const Exception& e): std::exception(), message_(e.what()/*message_*/) {
 	}
 
 	Exception::~Exception() throw() {
 	}
 
 	const char* Exception::what() const throw() {
-		return message_.str().c_str();
+		return message_.c_str();
 	}
 };
 
-SystemError::SystemError(): full_message_(std::ios_base::out | std::ios_base::app), errno_(errno) {
+SystemError::SystemError() : errno_(errno) {
+	message_.SetReserve(strlen(strerror(errno)) + 3);
 }
 
-SystemError::SystemError(const SystemError& e): Exception(e), full_message_(std::ios_base::out | std::ios_base::app), errno_(e.errno_) {
+SystemError::SystemError(const SystemError& e): Exception(e), errno_(e.errno_) {
 }
 
 SystemError::~SystemError() throw() {
 }
 
 const char* SystemError::what() const throw() {
-	full_message_.clear();
-	full_message_ << message_.str() << ": " << strerror(errno_);
-	return full_message_.str().c_str();
+	message_.AppendReserve(" (", 2);
+	message_.AppendReserve(strerror(errno), strlen(strerror(errno)));
+	message_.AppendReserve(")", 1);
+	return message_.c_str();
 }
