8df2f9ad8b4c8c1c23ab77d402919a7693fc583d
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-02-23 18:29:29 +0300

    Add simple Timer class

---
+++ libglosm-server/src/Timer.cc
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2010-2011 Dmitry Marakasov
+ *
+ * This file is part of glosm.
+ *
+ * glosm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * glosm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glosm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glosm/Timer.hh>
+
+Timer::Timer() {
+		Count();
+	}
+
+float Timer::Count() {
+	struct timeval current;
+	gettimeofday(&current, NULL);
+
+	float retval = (float)(current.tv_sec - last_.tv_sec) + (float)(current.tv_usec - last_.tv_usec)/1000000.0f;
+
+	last_ = current;
+
+	return retval;
+}
