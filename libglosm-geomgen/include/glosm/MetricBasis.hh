478ac648c514e67a907219388a2ecc7d267e2388
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-02-27 23:41:25 +0300

    Add MetricBasis to be used in geometry generation

---
+++ libglosm-geomgen/include/glosm/MetricBasis.hh
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2010-2011 Dmitry Marakasov
+ *
+ * This file is part of glosm.
+ *
+ * glosm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * glosm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glosm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef METRICBASIS_HH
+#define METRICBASIS_HH
+
+#include <glosm/GeometryOperations.hh>
+
+/**
+ * Convenient metric basis class for geometry construction
+ */
+class MetricBasis {
+public:
+	/**
+	 * Creates basis out of 3 axis vectors (expected to be normalized)
+	 */
+	MetricBasis(const Vector3i ref, const Vector3d& x, const Vector3d& y, const Vector3d& z);
+
+	/**
+	 * Creates basis out of a single axis vectors (expected to be normalized)
+	 *
+	 * Z axis is considered to be [0, 0, 1] and Y is calculated as
+	 * perpendicular to XZ plane
+	 */
+	MetricBasis(const Vector3i ref, const Vector3d& x);
+
+	/**
+	 * Returns basis-relative point as global fixed-point coordinates
+	 */
+	Vector3i Get(double x, double y, double z);
+
+protected:
+	Vector3i ref_;
+	Vector3d x_, y_, z_;
+};
+
+#endif
---
+++ libglosm-geomgen/src/MetricBasis.cc
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2010-2011 Dmitry Marakasov
+ *
+ * This file is part of glosm.
+ *
+ * glosm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * glosm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glosm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glosm/MetricBasis.hh>
+#include <glosm/GeometryOperations.hh>
+
+MetricBasis::MetricBasis(const Vector3i ref, const Vector3d& x, const Vector3d& y, const Vector3d& z):
+	ref_(ref),
+	x_(x),
+	y_(y),
+	z_(z) {
+}
+
+MetricBasis::MetricBasis(const Vector3i ref, const Vector3d& x):
+	ref_(ref),
+	x_(x),
+	y_(-x.CrossProduct(Vector3d(0.0, 0.0, 1.0))),
+	z_(Vector3d(0.0, 0.0, 1.0)) {
+}
+
+Vector3i MetricBasis::Get(double x, double y, double z) {
+	return FromLocalMetric(x_ * x + y_ * y + z_ * z, ref_);
+}
