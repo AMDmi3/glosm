490940270f590fd39257d426c16de57224b6c3b1
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-03-01 06:12:51 +0300

    Implement default BBox constructor properly

--- libglosm-server/include/glosm/BBox.hh
+++ libglosm-server/include/glosm/BBox.hh
@@ -48,7 +48,7 @@ public:
 	/**
 	 * Constructs empty bounding box
 	 */
-	BBox(): BBox(BBox<T>::Empty()) {
+	BBox(): left(std::numeric_limits<T>::max()), bottom(std::numeric_limits<T>::max()), right(std::numeric_limits<T>::min()), top(std::numeric_limits<T>::min()) {
 	}
 
 	/**
