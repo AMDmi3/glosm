b76f72ca27d917c6abf0f2479ee652a303641678
Dmitry Marakasov <amdmi3@amdmi3.ru>
2011-04-03 16:33:42 +0400

    Add simple guard class

---
+++ libglosm-server/include/glosm/Guard.hh
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2010-2011 Dmitry Marakasov
+ *
+ * This file is part of glosm.
+ *
+ * glosm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * glosm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with glosm.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GUARD_HH
+#define GUARD_HH
+
+#include <pthread.h>
+
+/**
+ * Simple guard class
+ */
+class Guard {
+public:
+	Guard(pthread_mutex_t& mutex);
+	~Guard();
+
+protected:
+	pthread_mutex_t& mutex_;
+};
+
+#endif
