74d1aa7a7b6df983845b27c1a4572c91216eb94c
Dmitry Marakasov <amdmi3@amdmi3.ru>
2012-04-08 22:38:35 +0400

    Rework id_map, add support for removing elements

--- libglosm-server/glosm/id_map.hh
+++ libglosm-server/glosm/id_map.hh
@@ -22,6 +22,7 @@
 #define ID_MAP_HH
 
 #include <vector>
+#include <cassert>
 
 /**
  * Custom std::map-like container for storing OSM data effeciently.
@@ -31,10 +32,8 @@
  *
  * Interface and usage semantics are the same as for std::map
  */
-template <typename I, typename T, int BUCKET_COUNT_ORDER = 0, int REHASH_GROWTH_ORDER = 1, int ITEMS_PER_CHUNK = 16*65536>
+template <typename I, typename T, int PAGE_SIZE = 1048576>
 class id_map {
-	static const size_t chunk_size = ITEMS_PER_CHUNK;
-
 public:
 	typedef I                                key_type;
 	typedef T                                mapped_type;
@@ -54,151 +53,252 @@ private:
 	typedef hash_node*                       hash_node_ptr;
 	typedef const hash_node*                 const_hash_node_ptr;
 
-public:
-	struct iterator {
-		typedef iterator _self;
-		typedef id_map<I, T, BUCKET_COUNT_ORDER, REHASH_GROWTH_ORDER, ITEMS_PER_CHUNK> _map;
+	class page {
+	public:
+		typedef hash_node* iterator;
+		typedef const hash_node* const_iterator;
 
-		typedef const _map*                       const_map_ptr;
+	public:
+		page() : count_(0), data_(reinterpret_cast<hash_node*>(::operator new(PAGE_SIZE))) {
+			assert(sizeof(hash_node) <= PAGE_SIZE);
+		}
 
-		typedef typename _map::pointer            pointer;
-		typedef typename _map::reference          reference;
-		typedef typename _map::hash_node_ptr      hash_node_ptr;
+		/* copy ctor and operator = transfer data ownership
+		 * this is rather ugly, but I it seems like the best way
+		 * to do it conveniently with stl containers (else, we'll
+		 * have to use vector of pointers and spend extra code on
+		 * exception safety and stuff */
+		page(const page& other) : count_(other.count_), data_(other.data_) {
+			other.count_ = 0;
+			other.data_ = NULL;
+		}
 
-		const_map_ptr map;
-		hash_node_ptr current;
+		page& operator=(const page& other) {
+			count_ = other.count_;
+			data_ = other.data_;
+			other.count_ = 0;
+			other.data_ = NULL;
 
-		iterator() : map(), current() {}
-		iterator(const_map_ptr m) : map(m), current() {}
-		iterator(const_map_ptr m, hash_node_ptr c) : map(m), current(c) {}
-
-		_self& operator++() {
-			if (current->next)
-				current = current->next;
-			else
-				current = map->findfirstafter(current->data.first);
 			return *this;
 		}
 
-		_self operator++(int) {
-			_self tmp = *this;
-			if (current->next)
-				current = current->next;
-			else
-				current = map->findfirstafter(current->data.first);
-			return tmp;
+		~page() {
+			if (data_) {
+				for (hash_node* i = data_; i < data_ + count_; ++i)
+					i->data.~value_type();
+				::operator delete(data_);
+			}
 		}
 
-		reference operator*() const {
-			return current->data;
+		inline size_t used() const { return count_; }
+		inline size_t free() const { return capacity() - count_; }
+		inline size_t capacity() const { return PAGE_SIZE/sizeof(hash_node); }
+
+		inline bool full() const { return free() == 0; }
+		inline bool empty() const { return count_ == 0; }
+
+		inline hash_node& front() const { return *data_; }
+		inline hash_node& back() const { return *(data_ + count_ - 1); }
+
+		iterator begin() { return data_; }
+		const_iterator begin() const { return data_; }
+
+		iterator end() { return data_ + count_; }
+		const_iterator end() const { return data_ + count_; }
+
+		hash_node* push(const value_type& v, hash_node* next = NULL) {
+			assert(free() > 0);
+
+			new(reinterpret_cast<void*>(&((data_ + count_)->data))) value_type(v);
+
+			(data_ + count_)->next = next;
+
+			return data_ + count_++;
 		}
 
-		pointer operator->() const {
-			return &current->data;
+		void pop() {
+			assert(count_ > 0);
+
+			data_[count_-1].data.~value_type();
+			--count_;
 		}
 
-		bool operator==(const _self& x) const {
-			return x.current == current;
-		}
-
-		bool operator!=(const _self& x) const {
-			return x.current != current;
-		}
+	private:
+		mutable int count_;
+		mutable hash_node* data_;
 	};
 
-	struct const_iterator {
-		typedef const_iterator _self;
-		typedef id_map<I, T, BUCKET_COUNT_ORDER, REHASH_GROWTH_ORDER, ITEMS_PER_CHUNK> _map;
+	typedef std::vector<page>                page_list;
 
-		typedef const _map*                        const_map_ptr;
+	typedef std::vector<hash_node*>          hashtable;
 
-		typedef typename _map::iterator            iterator;
+private:
+	size_t count_;
+	hashtable buckets_;
+	page_list pages_;
 
-		typedef typename _map::const_pointer       pointer;
-		typedef typename _map::const_reference     reference;
-		typedef typename _map::const_hash_node_ptr const_hash_node_ptr;
+public:
+	class iterator;
 
-		const_map_ptr map;
-		const_hash_node_ptr current;
+	class const_iterator {
+	public:
+		typedef const_iterator self;
 
-		const_iterator() : map(), current() {}
-		const_iterator(const_map_ptr m) : map(m) {}
-		const_iterator(const_map_ptr m, const_hash_node_ptr c) : map(m), current(c) {}
-		const_iterator(const iterator& it): map(it.map), current(it.current) {}
+	private:
+		typedef id_map<I, T, PAGE_SIZE>           map;
 
-		_self& operator++() {
-			if (current->next)
-				current = current->next;
+		typedef const map*                        const_map_ptr;
+
+		typedef typename map::iterator            iterator;
+
+		typedef typename map::const_pointer       pointer;
+		typedef typename map::const_reference     reference;
+		typedef typename map::const_hash_node_ptr const_hash_node_ptr;
+
+		const_map_ptr map_;
+		const_hash_node_ptr current_;
+
+	public:
+		const_iterator() : map_(), current_() {}
+		const_iterator(const_map_ptr m) : map_(m), current_() {}
+		const_iterator(const_map_ptr m, const_hash_node_ptr c) : map_(m), current_(c) {}
+		const_iterator(const iterator& it): map_(it.map_), current_(it.current_) {}
+
+		self& operator++() {
+			if (current_->next)
+				current_ = current_->next;
 			else
-				current = map->findfirstafter(current->data.first);
+				current_ = map_->findfirstafter(current_->data.first);
 			return *this;
 		}
 
-		_self operator++(int) {
-			_self tmp = *this;
-			if (current->next)
-				current = current->next;
+		self operator++(int) {
+			self tmp = *this;
+			if (current_->next)
+				current_ = current_->next;
 			else
-				current = map->findfirstafter(current->data.first);
+				current_ = map_->findfirstafter(current_->data.first);
 			return tmp;
 		}
 
-		reference operator*() const {
-			return current->data;
+		reference operator*() const { return current_->data; }
+		pointer operator->() const { return &current_->data; }
+
+		bool operator==(const self& x) const { return x.current_ == current_; }
+		bool operator!=(const self& x) const { return x.current_ != current_; }
+	};
+
+	class iterator {
+		friend class const_iterator;
+
+	public:
+		typedef iterator self;
+
+	private:
+		typedef id_map<I, T, PAGE_SIZE>          map;
+
+		typedef const map*                       const_map_ptr;
+
+		typedef typename map::pointer            pointer;
+		typedef typename map::reference          reference;
+		typedef typename map::hash_node_ptr      hash_node_ptr;
+
+	private:
+		const_map_ptr map_;
+		hash_node_ptr current_;
+
+	public:
+		iterator() : map_(), current_() {}
+		iterator(const_map_ptr m) : map_(m), current_() {}
+		iterator(const_map_ptr m, hash_node_ptr c) : map_(m), current_(c) {}
+
+		self& operator++() {
+			if (current_->next)
+				current_ = current_->next;
+			else
+				current_ = map_->findfirstafter(current_->data.first);
+			return *this;
 		}
 
-		pointer operator->() const {
-			return &current->data;
+		self operator++(int) {
+			self tmp = *this;
+			if (current_->next)
+				current_ = current_->next;
+			else
+				current_ = map_->findfirstafter(current_->data.first);
+			return tmp;
 		}
 
-		bool operator==(const _self& x) const {
-			return x.current == current;
-		}
+		reference operator*() const { return current_->data; }
+		pointer operator->() const { return &current_->data; }
 
-		bool operator!=(const _self& x) const {
-			return x.current != current;
-		}
+		bool operator==(const self& x) const { return x.current_ == current_; }
+		bool operator!=(const self& x) const { return x.current_ != current_; }
 	};
 
 public:
-	typedef std::vector<hash_node*> chunk_list;
-
-public:
-	id_map() {
-		init();
+	id_map(int nbuckets = 1024) : count_(0), buckets_(nbuckets, NULL) {
 	}
 
 	virtual ~id_map() {
-		deinit();
 	}
 
 	std::pair<iterator, bool> insert(const value_type& v) {
-		if (REHASH_GROWTH_ORDER > 0 && count > nbuckets * 2)
-			rehash(REHASH_GROWTH_ORDER);
+		if (count_ >= buckets_.size())
+			rehash(buckets_.size() * 2);
 
-		hash_node* t = reinterpret_cast<hash_node*>(alloc());
-		new((void*)&t->data)value_type(v);
+		if (pages_.empty() || pages_.back().full())
+			pages_.push_back(page());
 
-		/* No checking for existing element done - we assume OSM
-		 * data to not have objects with duplicate id's */
-		t->next = buckets[v.first & (nbuckets-1)];
-		buckets[v.first & (nbuckets-1)] = t;
-		count++;
+		int bucket = v.first & (buckets_.size() - 1);
+		buckets_[bucket] = pages_.back().push(v, buckets_[bucket]);
 
-		return std::make_pair(iterator(this, t), true);
+		++count_;
+
+		return std::make_pair(iterator(this, buckets_[bucket]), true);
 	}
 
-	size_t size() const {
-		return count;
+	/* erases last added element from the map
+	 * !! assumes that there's no other way for elements to be erased !!
+	 * if some other erase() is added, this should be rewritten
+	 */
+	void erase_last() {
+		assert(!pages_.empty()); /* external invaiant */
+		assert(!pages_.back().empty()); /* internal invariant */
+
+		/* remove element from the hash table */
+		int bucket = pages_.back().back().data.first & (buckets_.size() - 1);
+
+		assert(buckets_[bucket] == &pages_.back().back());
+
+		buckets_[bucket] = buckets_[bucket]->next;
+
+		/* remove element from the storage */
+		pages_.back().pop();
+		if (pages_.back().empty())
+			pages_.pop_back();
+
+		--count_;
+	}
+
+	inline size_t size() const {
+		return count_;
+	}
+
+	inline bool empty() const {
+		return count_ == 0;
 	}
 
 	void clear() {
-		deinit();
-		init();
+		buckets_.clear();
+		pages_.clear();
+		count_ = 0;
 	}
 
 	iterator find(key_type v) {
-		for (hash_node* n = buckets[v & (nbuckets-1)]; n; n = n->next)
+		int bucket = v & (buckets_.size() - 1);
+
+		for (hash_node* n = buckets_[bucket]; n; n = n->next)
 			if (n->data.first == v)
 				return iterator(this, n);
 
@@ -206,7 +306,9 @@ public:
 	}
 
 	const_iterator find(key_type v) const {
-		for (const hash_node* n = buckets[v & (nbuckets-1)]; n; n = n->next)
+		int bucket = v & (buckets_.size() - 1);
+
+		for (const hash_node* n = buckets_[bucket]; n; n = n->next)
 			if (n->data.first == v)
 				return const_iterator(this, n);
 
@@ -214,14 +316,14 @@ public:
 	}
 
 	iterator begin() {
-		if (count == 0)
+		if (count_ == 0)
 			return end();
 
 		return iterator(this, findfirst());
 	}
 
 	const_iterator begin() const {
-		if (count == 0)
+		if (count_ == 0)
 			return end();
 
 		return const_iterator(this, findfirst());
@@ -235,96 +337,57 @@ public:
 		return const_iterator(this, NULL);
 	}
 
+	void swap(id_map<I, T, PAGE_SIZE>& other) {
+		buckets_.swap(other.buckets_);
+		pages_.swap(other.pages_);
+		std::swap(count_, other.count_);
+	}
+
+	void rehash(size_t size) {
+		assert(size > 0);
+		assert((size & (size - 1)) == 0); // power of two
+
+		hashtable newbuckets(size, NULL);
+
+		for (typename page_list::iterator p = pages_.begin(); p != pages_.end(); ++p) {
+			for (typename page::iterator i = p->begin(); i != p->end(); ++i) {
+				int bucket = i->data.first & (newbuckets.size() - 1);
+				i->next = newbuckets[bucket];
+				newbuckets[bucket] = i;
+			}
+		}
+
+		buckets_.swap(newbuckets);
+	}
+
 protected:
 	hash_node* findfirst() const {
-		for (hash_node** b = buckets; b < buckets + nbuckets; ++b)
-			if (*b != NULL)
-				return *b;
+		for (typename hashtable::const_iterator i = buckets_.begin(); i != buckets_.end(); ++i)
+			if (*i != NULL)
+				return *i;
 
 		return NULL;
 	}
 
 	hash_node* findfirstfor(key_type v) const {
-		for (hash_node** b = buckets + (v & (nbuckets-1)); b < buckets + nbuckets; ++b)
-			if (*b != NULL)
-				return *b;
+		int bucket = v & (buckets_.size() - 1);
+
+		for (typename hashtable::const_iterator i = buckets_.begin() + bucket; i != buckets_.end(); ++i)
+			if (*i != NULL)
+				return *i;
 
 		return NULL;
 	}
 
 	hash_node* findfirstafter(key_type v) const {
-		for (hash_node** b = buckets + (v & (nbuckets-1)) + 1; b < buckets + nbuckets; ++b)
-			if (*b != NULL)
-				return *b;
+		int bucket = v & (buckets_.size() - 1);
+
+		for (typename hashtable::const_iterator i = buckets_.begin() + bucket + 1; i != buckets_.end(); ++i)
+			if (*i != NULL)
+				return *i;
 
 		return NULL;
 	}
-
-	hash_node* alloc() {
-		if (last_chunk_free == 0) {
-			/* chunk filled; allocate new one */
-			chunks.push_back(reinterpret_cast<hash_node*>(::operator new(chunk_size*sizeof(hash_node))));
-			current_ptr = chunks.back();
-			last_chunk_free = chunk_size;
-		}
-		hash_node* ret = current_ptr;
-		current_ptr++;
-		last_chunk_free--;
-		return ret;
-	}
-
-	void rehash(int k) {
-		int newnbuckets = nbuckets * (1 << k);
-
-		hash_node** newbuckets = new hash_node*[newnbuckets];
-		memset(newbuckets, 0, newnbuckets * sizeof(hash_node*));
-
-		for (hash_node** b = buckets; b < buckets + nbuckets; ++b) {
-			for (hash_node* n = *b; n != NULL; ) {
-				hash_node* cur = n;
-				n = n->next;
-				cur->next = newbuckets[cur->data.first & (newnbuckets-1)];
-				newbuckets[cur->data.first & (newnbuckets-1)] = cur;
-			}
-		}
-
-		nbuckets = newnbuckets;
-		delete[] buckets;
-		buckets = newbuckets;
-	}
-
-	void deinit() {
-		for (typename chunk_list::iterator c = chunks.begin(); c != chunks.end(); ++c) {
-			/* Call destructors for assigned elements */
-			for (hash_node* i = *c; i < ((*c == chunks.back()) ? (*c + chunk_size - last_chunk_free) : (*c + chunk_size)); ++i) {
-				i->data.~value_type();
-			}
-			::operator delete(*c);
-		}
-
-		chunks.clear();
-
-		delete[] buckets;
-	}
-
-	void init() {
-		count = 0;
-		last_chunk_free = 0;
-		nbuckets = 1 << BUCKET_COUNT_ORDER;
-		buckets = new hash_node*[nbuckets];
-		memset(buckets, 0, nbuckets * sizeof(hash_node*));
-	}
-
-protected:
-	/* hash table */
-	size_t nbuckets; /* always power of 2, so nbuckets-1 is bitmask for hashing */
-	hash_node** buckets;
-	size_t count; /* @todo superfluous - may be derived from nchunks and urrent_ptr */
-
-	/* memory pool */
-	chunk_list chunks;
-	size_t last_chunk_free;
-	hash_node* current_ptr; /* @todo superfluous - either last_chunk free or current_ptr should be removed */
 };
 
 #endif
