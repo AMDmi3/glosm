519d415126ea3cd71c18a6cc56b02f9496e57aaa
Dmitry Marakasov <amdmi3@amdmi3.ru>
2012-04-09 19:32:37 +0400

    Use new id_map functionality to drop ways with nonexisting nodes
    This allows to load broken/incomplete dumps

--- libglosm-server/PreloadedXmlDatasource.cc
+++ libglosm-server/PreloadedXmlDatasource.cc
@@ -47,6 +47,7 @@
 #include <glosm/ParsingHelpers.hh>
 
 #include <cstdlib>
+#include <iostream>
 
 PreloadedXmlDatasource::PreloadedXmlDatasource() : XMLParser(XMLParser::HANDLE_ELEMENTS), bbox_(BBoxi::Empty()) {
 }
@@ -104,61 +105,67 @@ void PreloadedXmlDatasource::StartElement(const char* name, const char** atts) {
 			bbox_.Include(ParseBound(atts));
 		}
 	} else if (tag_level_ == 2 && current_tag_ == NODE) {
-		if (StrEq<0>(name, "tag")) {
-//			if (last_node_tags_ == node_tags_.end()) {
-//				std::pair<NodeTagsMap::iterator, bool> p = node_tags_.insert(std::make_pair(last_node_->first, TagsMap()));
-//				last_node_tags_ = p.first;
-//			}
-//			ParseTag(last_node_tags_->second, atts);
-		} else {
-			throw ParsingException() << "unexpected tag in node";
+		if (last_node_ != nodes_.end()) {
+			if (StrEq<0>(name, "tag")) {
+//				if (last_node_tags_ == node_tags_.end()) {
+//					std::pair<NodeTagsMap::iterator, bool> p = node_tags_.insert(std::make_pair(last_node_->first, TagsMap()));
+//					last_node_tags_ = p.first;
+//				}
+//				ParseTag(last_node_tags_->second, atts);
+			} else {
+				throw ParsingException() << "unexpected tag in node";
+			}
 		}
 	} else if (tag_level_ == 2 && current_tag_ == WAY) {
-		if (StrEq<1>(name, "tag")) {
-			ParseTag(last_way_->second.Tags, atts);
-		} else if (StrEq<1>(name, "nd")) {
-			int id;
+		if (last_way_ != ways_.end()) {
+			if (StrEq<1>(name, "tag")) {
+				ParseTag(last_way_->second.Tags, atts);
+			} else if (StrEq<1>(name, "nd")) {
+				int id;
 
-			if (**atts && StrEq<0>(*atts, "ref"))
-				id = strtol(*(atts+1), NULL, 10);
-			else
-				throw ParsingException() << "no ref attribute for nd tag";
+				if (**atts && StrEq<0>(*atts, "ref"))
+					id = strtol(*(atts+1), NULL, 10);
+				else
+					throw ParsingException() << "no ref attribute for nd tag";
 
-			last_way_->second.Nodes.push_back(id);
-		} else {
-			throw ParsingException() << "unexpected tag in way";
+				last_way_->second.Nodes.push_back(id);
+			} else {
+				throw ParsingException() << "unexpected tag in way";
+			}
 		}
 	} else if (tag_level_ == 2 && current_tag_ == RELATION) {
-		if (StrEq<1>(name, "tag")) {
-//			ParseTag(last_relation_->second.Tags, atts);
-		} else if (StrEq<1>(name, "member")) {
-			int ref;
-			const char* role;
-			Relation::Member::Type_t type;
+		if (last_relation_ != relations_.end()) {
+			if (StrEq<1>(name, "tag")) {
+//				ParseTag(last_relation_->second.Tags, atts);
+			} else if (StrEq<1>(name, "member")) {
+				int ref;
+				const char* role;
+				Relation::Member::Type_t type;
 
-			for (const char** att = atts; *att; ++att) {
-				if (StrEq<2>(*att, "ref"))
-					ref = strtol(*(++att), NULL, 10);
-				else if (StrEq<1>(*att, "type")) {
-					++att;
-					if (StrEq<1>(*att, "node"))
-						type = Relation::Member::NODE;
-					else if (StrEq<1>(*att, "way"))
-						type = Relation::Member::WAY;
-					else if (StrEq<1>(*att, "relation"))
-						type = Relation::Member::RELATION;
-					else
-						throw ParsingException() << "bad relation member role";
-				} else if (StrEq<2>(*att, "role")) {
-					role = *(++att);
-				} else {
-					throw ParsingException() << "unexpected attribute in relation member";
+				for (const char** att = atts; *att; ++att) {
+					if (StrEq<2>(*att, "ref"))
+						ref = strtol(*(++att), NULL, 10);
+					else if (StrEq<1>(*att, "type")) {
+						++att;
+						if (StrEq<1>(*att, "node"))
+							type = Relation::Member::NODE;
+						else if (StrEq<1>(*att, "way"))
+							type = Relation::Member::WAY;
+						else if (StrEq<1>(*att, "relation"))
+							type = Relation::Member::RELATION;
+						else
+							throw ParsingException() << "bad relation member role";
+					} else if (StrEq<2>(*att, "role")) {
+						role = *(++att);
+					} else {
+						throw ParsingException() << "unexpected attribute in relation member";
+					}
 				}
-			}
 
-			last_relation_->second.Members.push_back(Relation::Member(type, ref, role));
-		} else {
-			throw ParsingException() << "unexpected tag in relation";
+				last_relation_->second.Members.push_back(Relation::Member(type, ref, role));
+			} else {
+				throw ParsingException() << "unexpected tag in relation";
+			}
 		}
 	} else if (tag_level_ == 0 && current_tag_ == NONE && StrEq<-1>(name, "osm")) {
 		current_tag_ = OSM;
@@ -173,36 +180,47 @@ void PreloadedXmlDatasource::EndElement(const char* /*name*/) {
 	if (tag_level_ == 2) {
 		switch (current_tag_) {
 		case WAY:
-			/* check if a way is closed */
-			if (last_way_->second.Nodes.front() == last_way_->second.Nodes.back()) {
-				last_way_->second.Closed = true;
+			if (last_way_ != ways_.end()) try {
+				/* check if a way is closed */
+				if (last_way_->second.Nodes.front() == last_way_->second.Nodes.back()) {
+					last_way_->second.Closed = true;
 
-				/* check if a way is clockwise */
-				NodesMap::const_iterator prev, cur;
-				osmlong_t area = 0;
-				for (Way::NodesList::const_iterator i = last_way_->second.Nodes.begin(); i != last_way_->second.Nodes.end(); ++i) {
-					cur = nodes_.find(*i);
-					if (cur == nodes_.end())
-						throw ParsingException() << "node " << *i << " referenced by way " << last_way_->first << " was not found in this dump";
-					if (i != last_way_->second.Nodes.begin())
-						area += (osmlong_t)prev->second.Pos.x * cur->second.Pos.y - (osmlong_t)cur->second.Pos.x * prev->second.Pos.y;
-					prev = cur;
-					last_way_->second.BBox.Include(cur->second.Pos);
-				}
+					/* check if a way is clockwise */
+					NodesMap::const_iterator prev, cur;
+					osmlong_t area = 0;
+					for (Way::NodesList::const_iterator i = last_way_->second.Nodes.begin(); i != last_way_->second.Nodes.end(); ++i) {
+						cur = nodes_.find(*i);
+						if (cur == nodes_.end())
+							throw DataException() << "node " << *i << " referenced by way " << last_way_->first << " was not found in this dump";
+						if (i != last_way_->second.Nodes.begin())
+							area += (osmlong_t)prev->second.Pos.x * cur->second.Pos.y - (osmlong_t)cur->second.Pos.x * prev->second.Pos.y;
+						prev = cur;
+						last_way_->second.BBox.Include(cur->second.Pos);
+					}
 
-				last_way_->second.Clockwise = area < 0;
-			} else {
-				for (Way::NodesList::const_iterator i = last_way_->second.Nodes.begin(); i != last_way_->second.Nodes.end(); ++i) {
-					NodesMap::const_iterator cur = nodes_.find(*i);
-					if (cur == nodes_.end())
-						throw ParsingException() << "node " << *i << " referenced by way " << last_way_->first << " was not found in this dump";
-					last_way_->second.BBox.Include(cur->second.Pos);
+					last_way_->second.Clockwise = area < 0;
+				} else {
+					for (Way::NodesList::const_iterator i = last_way_->second.Nodes.begin(); i != last_way_->second.Nodes.end(); ++i) {
+						NodesMap::const_iterator cur = nodes_.find(*i);
+						if (cur == nodes_.end())
+							throw DataException() << "node " << *i << " referenced by way " << last_way_->first << " was not found in this dump";
+						last_way_->second.BBox.Include(cur->second.Pos);
+					}
 				}
+			} catch (DataException& e) {
+				std::cerr << "WARNING: " << e.what() << ", way dropped" << std::endl;
+				ways_.erase_last();
 			}
-			/* fallthrough */
+			last_way_ = ways_.end();
+			current_tag_ = OSM;
+			break;
 		case NODE:
+			current_tag_ = OSM;
+			last_node_ = nodes_.end();
+			break;
 		case RELATION:
 			current_tag_ = OSM;
+			last_relation_ = relations_.end();
 			break;
 		default:
 			break;
